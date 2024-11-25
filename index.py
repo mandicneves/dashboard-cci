384486243591561f4200431940517607331c5d69
mandicneves <mandicneves@gmail.com>
2024-11-25 11:40:39 -0300

    maps on

--- index.py
+++ index.py
@@ -272,7 +272,7 @@ def update_checklist(selected_values):
 def update_graficos_geral_vs(selected_values, operacao):
 
     # chave api
-    # px.set_mapbox_access_token(open("./keys/mapbox_token").read())
+    px.set_mapbox_access_token(open("./keys/mapbox_token").read())
     
     # filtrando datasets
     df_seguidores = seguidores[seguidores["NOME"].isin(list(selected_values))]
